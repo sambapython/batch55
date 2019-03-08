2c5124a4f0f42cf5f9f9be8ac3c84d19664aa912
sambapython@gmail.com <sambapython@gmail.com>
2019-03-08 09:26:27 +0530

    2019-03-08 dictionaries.

---
+++ dict_1.py
@@ -0,0 +1,6 @@
+emp={"name":"jayaram","id":23,"doj":"2018-06-30","salary":"10L"}
+address={"street":"Anjaneyanagar","city":"hyderabad","area":"moosapet"}
+print(emp)
+print(address)
+for i in emp:
+	print(i)
