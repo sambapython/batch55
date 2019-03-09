c08a38de18d3cd8324c13ad4fd5a5c358bacac15
sambapython@gmail.com <sambapython@gmail.com>
2019-03-09 10:09:17 +0530

    2019-03-09 file as a module.

---
+++ modules/app.py
@@ -0,0 +1,24 @@
+'''
+import sales
+sales.create_customer()
+sales.sales_process()
+sales.sales_delivery()
+'''
+'''
+import sales as s
+s.create_customer()
+s.sales_process()
+s.sales_delivery()
+'''
+#import sales
+'''
+import sales
+import pur
+sales.create_customer()
+print(sales.a)
+print(sales.b)
+print(sales.c)
+pur.create_suplier()
+'''
+import sales
+import pur
---
+++ modules/sales.py
@@ -0,0 +1,12 @@
+print("this is sales file started this is modified again..")
+a=1000
+b=2000
+c=a+b
+def create_customer():
+	print("this is customer creation")
+def sales_process():
+	print("sales process")
+def sales_delivery():
+	print("sales delivery")
+print("this is sales file ended")
+#create_customer()
